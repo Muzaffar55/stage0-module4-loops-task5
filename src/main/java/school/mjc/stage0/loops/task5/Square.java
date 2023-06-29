4d76d288a3adcc195710dce23834d1bbd7f5e5d2
Muzaffar55 <97522350+Muzaffar55@users.noreply.github.com>
2023-06-29 22:00:04 +0500

    Update Square.java
    Done

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -5,18 +5,18 @@ public class Square {
         printSquareFrom8s(2);
     }
 
-    public void printSquareFrom8s(int sideLength) {
+    public static void printSquareFrom8s(int sideLength) {
         for (int i = 0; i < sideLength; i++) {
             for (int j = 0; j < sideLength; j++) {
                 if (i == 0 || i == sideLength - 1) {
                     System.out.print(8);
-                } else if (j == 0 || j == sideLength - 1) {
+                } else  if (j == 0 || j == sideLength - 1) {
                     System.out.print(8);
                 } else {
                     System.out.print(" ");
                 }
             }
-            System.out.print();
+            System.out.println();
         }
     }
 }
